67f1f56fe9aae8289022818c9454c70c576e5335
Nikushaana <nikanashvili@gmail.com>
2024-09-17 10:28:04 +0400

    fix: just some bugs for build

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ShareStatesChild>
-      <Router>
-        <EstateContext>
+      <EstateContext>
+        <Router>
           <App />
-        </EstateContext>
-      </Router>
+        </Router>
+      </EstateContext>
     </ShareStatesChild>
   </React.StrictMode>
 );
