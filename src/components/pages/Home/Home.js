fa6172cb5b26063b7d673a48a083f713d2755b5d
Nikushaana <nikanashvili@gmail.com>
2024-09-19 03:09:34 +0400

    fix: small bug

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import PropertyCard from "../../cards/PropertyCard";
 import { ShareStatesCont } from "../../contexts/sharedStates";
 import Button1 from "../../buttons/button1";
